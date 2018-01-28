158e642e0b70848d6967a9b19c0c88248284c6a2
Anish Kannan <anikan33@gmail.com>
2018-01-27 16:23:05 -0800

    Added beginning of damage system and pulsing trail

---
+++ Assets/Scripts/EnemySignal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySignal : MonoBehaviour {
+
+    public float damageValue = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
